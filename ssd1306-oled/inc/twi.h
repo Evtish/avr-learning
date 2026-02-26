a3816fdbad448ddca95907572fb908373176ad63
evtish <evtish@proton.me>
2026-02-26 20:04:52 +0300

    ssd1306-oled initial

---
+++ ssd1306-oled/inc/twi.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <stdint.h>
+
+#define TWI_SUCCESS 0xFF // because 0x00 is a TWI status code
+
+void twi_init(void);
+uint8_t twi_receive_bytes(uint8_t *buf, const uint8_t start_address, const uint8_t amount_of_bytes);
+uint8_t twi_transmit_bytes(const uint8_t *buf, const uint8_t start_address, const uint8_t amount_of_bytes);
---
+++ ssd1306-oled/src/main.c
@@ -0,0 +1,16 @@
+#include <stdbool.h>
+#include <stdint.h>
+
+#include <avr/io.h>
+
+#include "twi.h"
+
+int main(void) {
+	twi_init();
+
+	while (true) {
+		
+	}
+
+	return 0;
+}
